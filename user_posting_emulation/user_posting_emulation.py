19184bee3503bdb2158c465d0d25b425bdf18973
pili-zhangqiu <p.zhangqiu@gmail.com>
2024-01-07 20:13:46 +0000

    feat(class): Rename class and add functions
    - Add functions to retrieve data from emulation database to class
    - Separate function to retrieve and print data to console

--- user_posting_emulation/user_posting_emulation.py
+++ user_posting_emulation/user_posting_emulation.py
@@ -14,8 +14,11 @@ import os
 random.seed(100)
 
 
-class AWSDBConnector:
+class DataEmulator:
     def __init__(self, credentials_filepath: str) -> None:
+        '''
+        Initialise DataEmulator class and load class variables
+        '''
         # Get credentials
         self.credentials_filepath = credentials_filepath
         self.credentials = self.read_db_creds()
@@ -62,49 +65,80 @@ class AWSDBConnector:
         try: 
             engine = sqlalchemy.create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4")
             print(f"Connection to the '{host}' for user '{user}' created successfully.")
+            print("---")
             return engine
 
         except Exception as ex:
             print("Connection could not be made due to the following error: \n", ex)
+            print("---")
 
 
-def run_infinite_post_data_loop():
-    while True:
+    def get_emulation_data(self) -> dict:
+        '''
+        Retrieve data from a random row from the emulation database.
+
+        Returns:
+        -------
+        result: dict
+            Random emulation data in dict format. Keys: "pin", "geo", "user".
+        '''
         sleep(random.randrange(0, 2))
         random_row = random.randint(0, 11000)
-        engine = new_connector.create_db_connector()
 
-        with engine.connect() as connection:
-
+        with self.engine.connect() as connection:
+            # Get pin data
             pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
             pin_selected_row = connection.execute(pin_string)
-            
+                
             for row in pin_selected_row:
                 pin_result = dict(row._mapping)
 
+            # Get geolocation data
             geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
             geo_selected_row = connection.execute(geo_string)
             
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
 
+            # Get user data
             user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
             user_selected_row = connection.execute(user_string)
-            
+                
             for row in user_selected_row:
                 user_result = dict(row._mapping)
             
-            print(pin_result)
-            print(geo_result)
-            print(user_result)
+            # Construct dictionary
+            result = {"pin": pin_result, "geo": geo_result, "user": user_result}
+            
+            return result
+    
+    def print_to_console(self) -> None:   
+        '''
+        Run an emulation data query and print the emulated data.
+        ''' 
+        # Get data
+        emulation_row = self.get_emulation_data()
+        
+        # Print data
+        print("Pin data:")     
+        print(emulation_row["pin"])
+        
+        print("\nGeolocation data:")     
+        print(emulation_row["geo"])
+        
+        print("\nUser data:")     
+        print(emulation_row["user"])
+        
+        print("---")
 
 
 if __name__ == "__main__":
     # Initialise database connector to retrieve emulation data
     directory = os.path.dirname(os.path.abspath(__file__))
     credentials_filepath = directory + "/" + "db_creds_aws_emulation.yaml"
-    new_connector = AWSDBConnector(credentials_filepath)
+    emulator = DataEmulator(credentials_filepath)
 
     # Print emulation to console
-    run_infinite_post_data_loop()
+    while True:
+        emulator.print_to_console()
     
